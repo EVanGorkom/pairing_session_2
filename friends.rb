e6e6e90fb8ef7c8678d696b36d854c8eb2fe2abe
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-06-22 18:00:17 -0500

    Add arrays

---
+++ friends.rb
@@ -0,0 +1,8 @@
+arrays = ["Will", "Ethan V", "Lane", "Charles", "Ethan B"]
+puts arrays[2]
+puts arrays[4]
+puts arrays[0]
+
+spots = ["Japan", "Portugal", "Ireland", "South Korea", "Chile"]
+sample = spots.sample(2)
+print sample
